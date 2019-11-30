d56d2ac8ffb4cb9bb5f9f2008fe5629d483e4610
kunal <kunalkumar.kus@gmail.com>
2019-12-01 01:12:52 +0530

    try and expect added for image folder

--- python/main.py
+++ python/main.py
@@ -3,6 +3,10 @@ from pdf2image import convert_from_path
 
 def usingpdf2img():
     pages = convert_from_path("../Akash Deep_CV.pdf",100) # reading pdf in 100 dpi
+    try: #if image folder not availble 
+        os.mkdir("image")
+    except:
+        print("already exist")
     for page in pages:
         page.save("image/{}.tiff".format(page),'TIFF',quality = 10) #saving
 
