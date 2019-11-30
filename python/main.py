006c9a333439d04680da7752f5a63350ffe00af2
kunal <kunalkumar.kus@gmail.com>
2019-12-01 01:08:23 +0530

    converting pdf to img

--- python/main.py
+++ python/main.py
@@ -2,9 +2,9 @@ import os
 from pdf2image import convert_from_path
 
 def usingpdf2img():
-    pages = convert_from_path("../Akash Deep_CV.pdf",500) # reading pdf in 500 dpi
+    pages = convert_from_path("../Akash Deep_CV.pdf",100) # reading pdf in 100 dpi
     for page in pages:
-        page.save("image/{}.tiff".format(page),'TIFF') #saving
+        page.save("image/{}.tiff".format(page),'TIFF',quality = 10) #saving
 
 if __name__ == "__main__":
     usingpdf2img()
