92c0d31c246773af720690cf74a1a5c8d54f18b0
kunal <kunalkumar.kus@gmail.com>
2019-12-01 01:52:32 +0530

    seconf logic using PyMuPDF

---
+++ python/main_2.py
@@ -0,0 +1,12 @@
+import fitz
+import os
+from PIL import Image
+open_doc = fitz.open("../Akash Deep_CV.pdf")
+for page in range(0,open_doc.pageCount):
+    pages = open_doc[page]
+    pix = pages.getPixmap()
+    pix.writeImage("{}.png".format(page))
+    image = Image.open("%s.png"%(page))
+    image.save("%s.tiff"%(page),"TIFF",quality=10)
+    os.remove("%s.png"%(page))
+    
