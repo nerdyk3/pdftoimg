6c098ffc939f34469386223976a33702a6eb25e5
kunal <kunalkumar.kus@gmail.com>
2019-12-01 02:01:05 +0530

    comment added

--- python/main_2.py
+++ python/main_2.py
@@ -3,14 +3,18 @@ import os
 from PIL import Image
 
 def pdf2img():
-    open_doc = fitz.open("../Akash Deep_CV.pdf")
-    for page in range(0,open_doc.pageCount):
-        pages = open_doc[page]
-        pix = pages.getPixmap()
-        pix.writeImage("{}.png".format(page))
-        image = Image.open("%s.png"%(page))
-        image.save("%s.tiff"%(page),"TIFF",quality=10)
-        os.remove("%s.png"%(page))
+    open_doc = fitz.open("../Akash Deep_CV.pdf") #reading pdf
+    try: #if image folder not availble 
+        os.mkdir("image_2")
+    except:
+        print("already exist")
+    for page in range(0,open_doc.pageCount): #runing on per page
+        pages = open_doc[page] #opening of doc
+        pix = pages.getPixmap() #making frame
+        pix.writeImage("image_2/{}.png".format(page)) #saving asimage
+        image = Image.open("image_2/%s.png"%(page)) #open image
+        image.save("image_2/%s.tiff"%(page),"TIFF",quality=10) #saving in tiff
+        os.remove("image_2/%s.png"%(page)) #deleting png
         
 if __name__ == "__main__":
     pdf2img()
