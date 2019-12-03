efb7c5d0e259da699e0343ea6739251a7b009f41
ritu bind <ritubind1211@gmail.com>
2019-12-03 20:35:28 +0530

    comment modified

--- python/main.py
+++ python/main.py
@@ -1,17 +1,21 @@
 import os
 from pdf2image import convert_from_path
 
+
 def usingpdf2img():
+    '''funtion used for convert a pdf into tiff'''
     file_path = input("enter your file path:")
-    pages = convert_from_path(file_path,100) # reading pdf in 100 dpi
-    try: #if image folder not availble 
+    pages = convert_from_path(file_path, 100)  # reading pdf in 100 dpi
+    try:
         os.mkdir("image")
     except:
         pass
-    page_number = 0 #init page number
+    page_number = 0
     for page in pages:
-        page_number +=1 #increasing page number
-        page.save("image/{}.tiff".format(page_number),'TIFF',quality = 10) #saving
+        page_number += 1
+        page.save("image/{}.tiff".format(page_number),
+                  'TIFF', quality=10)  # saving
+
 
 if __name__ == "__main__":
     usingpdf2img()
--- python/main_2.py
+++ python/main_2.py
@@ -2,20 +2,24 @@ import fitz
 import os
 from PIL import Image
 
+
 def pdf2img():
+    '''funtion used for convert a pdf into tiff'''
     file_path = input("enter your file path:")
-    open_doc = fitz.open(file_path) #reading pdf
-    try: #if image folder not availble 
+    open_doc = fitz.open(file_path)
+    try:
         os.mkdir("image_2")
     except:
         pass
-    for page in range(0,open_doc.pageCount): #runing on per page
-        pages = open_doc[page] #opening of doc
-        pix = pages.getPixmap() #making frame
-        pix.writeImage("image_2/{}.png".format(page)) #saving asimage
-        image = Image.open("image_2/%s.png"%(page)) #open image
-        image.save("image_2/%s.tiff"%(page),"TIFF",quality=10) #saving in tiff
-        os.remove("image_2/%s.png"%(page)) #deleting png
-        
+    for page in range(0, open_doc.pageCount):
+        pages = open_doc[page]
+        pix = pages.getPixmap()
+        pix.writeImage("image_2/{}.png".format(page))
+        image = Image.open("image_2/%s.png" % (page))
+        image.save("image_2/%s.tiff" %
+                   (page), "TIFF", quality=10)
+        os.remove("image_2/%s.png" % (page))
+
+
 if __name__ == "__main__":
     pdf2img()
